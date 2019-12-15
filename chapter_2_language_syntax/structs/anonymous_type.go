5b059340b5d08ccb6454c20f24d165af652db34b
aakash10292 <aakash10292@gmail.com>
2019-12-15 17:26:59 -0500

    added example demponstraing anonymous type with literal struct init using :=

--- chapter_2_language_syntax/structs/anonymous_type.go
+++ chapter_2_language_syntax/structs/anonymous_type.go
@@ -1,14 +1,31 @@
 package main
+
 import "fmt"
 
-func main(){
-    // Declaring a variable e1 based on a literal struct type
-    // We're using var so obviously initialisation it with zero-value
-    var e1 struct {
-        flag bool
-        counter int16
-        pi float32
-    }
-    
-    fmt.Printf("%+v\n",e1)
+func main() {
+	// Declaring a variable e1 based on an anonymous literal struct type
+	// We're using var so obviously initialisation it with zero-value
+	var e1 struct {
+		flag    bool
+		counter int16
+		pi      float32
+	}
+
+	fmt.Printf("%+v\n", e1)
+
+	// Declaring a variable of an anonymous type and init with a struct literal
+	e2 := struct {
+		flag    bool //Notice , no commas during type declaration
+		counter int16
+		pi      float32
+	}{
+		flag:    true,
+		counter: 10, //Notice, commas used during value construction
+		pi:      3.141592,
+	}
+	// A good use case for anonymous literal types : json unmarshalling, when we need type information only for this one place.
+	// We wouldn't want to name something we'd be using in only one place (that would be pollution). Best to use above in such a case
+
+	fmt.Printf("%+v\n", e2)
+
 }

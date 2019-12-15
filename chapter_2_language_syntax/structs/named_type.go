11b384e3229d227ed25e888b849a1bfa96612ea5
aakash10292 <aakash10292@gmail.com>
2019-12-15 17:02:59 -0500

    demonstration of value construction using struct literals and :=

--- chapter_2_language_syntax/structs/named_type.go
+++ chapter_2_language_syntax/structs/named_type.go
@@ -13,4 +13,17 @@ func main() {
 	var e1 example // we used var. So, obviously we are declaring a variable of type example and setting it to it's zero value
 	//Displaying the value now
 	fmt.Printf("%+v\n", e1)
+
+	// Declaring a variable of type example and init using a struct literal.
+	// Remember we can use the := operator when we want to declare an init with a non-zero value.
+	e2 := example{
+		flag:    true,
+		counter: 10,
+		pi:      3.1415,
+	}
+
+	//We can display individual field values using the . operator"
+	fmt.Println("Flag ", e2.flag)
+	fmt.Println("Counter ", e2.counter)
+	fmt.Println("pi ", e2.pi)
 }

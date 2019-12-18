cdd7f667f7a5084b098ba4d90b267defe8c6e1a2
aakash10292 <aakash10292@gmail.com>
2019-12-17 22:43:23 -0500

    added example demonstrating 256 bit precision of KIND constants

---
+++ chapter_2_language_syntax/constants/constants_type_vs_kind_2.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+const (
+    // Max integer value on 64 bit architecture
+    maxInt = 9223372036854775807
+
+    // Much larger value than int64
+    bigger = 9223372036854775808543522345
+
+    // Comment out the below line .. will NOT compile
+    biggerInt int64 =  9223372036854775808543522345
+   )
+

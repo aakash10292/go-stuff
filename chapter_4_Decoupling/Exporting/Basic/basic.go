05e0d9f51d8e737851010489ea207e56a30f1173
aakash10292 <aakash10292@gmail.com>
2020-01-02 16:52:49 -0500

    added comments to explain lowercase/unexported identifiers

--- chapter_4_Decoupling/Exporting/Basic/basic.go
+++ chapter_4_Decoupling/Exporting/Basic/basic.go
@@ -8,4 +8,15 @@ import (
 func main() {
 	Counter := counter.AlertCounter(10)
 	fmt.Printf("Counter: %d\n", Counter)
+    /*
+    open counter/counter.go and rename AlertCounter to alertCounter
+    Compile, and run basic.go.
+    You should see an error
+    // ./basic.go:9: cannot refer to unexported name counters.alertCounter
+	// ./basic.go:9: undefined: counters.alertCounter
+
+    This is because identifiers that start with a LOWERCASE LETTER ARE UNEXPORTED.
+    In this case, main cannot access as unexported identifier from counter package.
+    Hence, the error. 
+    */
 }
